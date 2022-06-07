87cea56129dd98ecf6e107956699102d09bad1dc
thisIsJooS <thisis.joos@gmail.com>
2022-06-07 15:11:24 +0900

    html form

--- Blogproject/Blogproject/urls.py
+++ Blogproject/Blogproject/urls.py
@@ -19,5 +19,7 @@ from blogapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
 ]
--- Blogproject/blogapp/views.py
+++ Blogproject/blogapp/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Blog
+from django.utils import timezone
 
-# Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+# 블로그 글 작성 
+def new(request):
+    return render(request, 'new.html')
+
+# 블로그 글 저장
+def create(request):
+    if request.method == 'POST':
+        post = Blog()
+        post.title = request.POST['title']
+        post.body = request.POST['body']
+        post.date = timezone.now()
+        post.save()
+    return redirect('home')
